07442e69462479a3ff6ca3f91879f22f611bef60
Sergiu Coshciug <sergiucoshciug@gmail.com>
2015-09-03 00:55:40 +0300

    implemented autoplay.

--- src/OneRepeat/wwwroot/_references.js
+++ src/OneRepeat/wwwroot/_references.js
@@ -1,4 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="../gulpfile - copy.js" />
+/// <reference path="../Gruntfile.js" />
 /// <reference path="../gulpfile.js" />
 /// <reference path="js/site.js" />
